a78ffb81562b2ffb943adbd6cf2cd4253e0a9113
wangws1990 <wangws1990@163.com>
2019-06-03 20:19:37 +0800

    工程创建

---
+++ GKSongApp/GKSongApp/Class/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GKSongApp
+//
+//  Created by wangws1990 on 2019/6/3.
+//  Copyright © 2019 王炜圣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GKSongApp/GKSongApp/Class/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GKSongApp
+//
+//  Created by wangws1990 on 2019/6/3.
+//  Copyright © 2019 王炜圣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
